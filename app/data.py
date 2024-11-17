c89092dad7d05669e42ac88f99d66fe73d3cf873
Juan Sebastian Camacho <jscamachop13@gmail.com>
2024-11-17 12:51:17 -0500

    Addicion de las fotos, cambios en el html y css y dockerfile

--- app/data.py
+++ app/data.py
@@ -1,19 +1,58 @@
 pokeneas = [
     {
-        "id": 1,
-        "nombre": "TU",
-        "altura": "1.75m",
-        "habilidad": "Super Salto",
-        "imagen": "https://example.com/imagen1.png",  # Reemplaza con un enlace real o usa tus imágenes
-        "frase_filosofica": "El que madruga encuentra los mejores saltos."
+        "id": 468,
+        "nombre": "Togekiss",
+        "altura": "1.5m",
+        "habilidad": "Entusiasmo",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/Togekiss.png",  
+        "frase_filosofica": "Soy pureza alada, un ángel de algodón de azúcar."
     },
     {
-        "id": 2,
-        "nombre": "YO",
-        "altura": "1.60m",
-        "habilidad": "Invisibilidad",
-        "imagen": "https://example.com/imagen2.png",
-        "frase_filosofica": "A veces, es mejor no ser visto."
+        "id": 658,
+        "nombre": "Greninja",
+        "altura": "1.5m",
+        "habilidad": "Torrente",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/Greninja.png",
+        "frase_filosofica": "Soy pureza alada, un ángel de algodón de azúcar."
     },
-    # Agrega entre 7 y 10 Pokeneas más aquí
+    {
+        "id": 392,
+        "nombre": "Infernape",
+        "altura": "1.2m",
+        "habilidad": "Mar Llamas",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/Infernape.png",
+        "frase_filosofica": "Fuego y pasion, asi soy yo"
+    },
+    {
+        "id": 130,
+        "nombre": "Gyarados",
+        "altura": "6.5m",
+        "habilidad": "Intimidacion",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/Gyarados.png",
+        "frase_filosofica": "Las profundidades me conocen, el poder me obedece."
+    },
+    {
+        "id": 282,
+        "nombre": "Gardevoir",
+        "altura": "1.6m",
+        "habilidad": "Sincronia",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/Gardevoir.png",
+        "frase_filosofica": "Elegancia y poder, una combinación letal."
+    },
+    {
+        "id": 873,
+        "nombre": "Frosmoth",
+        "altura": "1.3m",
+        "habilidad": "Polvo Escudo",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/frosmoth.png",
+        "frase_filosofica": "El frío es mi abrigo, la calma mi alma."
+    },
+    {
+        "id": 94,
+        "nombre": "Gengar",
+        "altura": "1.5m",
+        "habilidad": "Cuerpo Maldito",
+        "imagen": "https://storage.googleapis.com/jscamachobucket/gengar.png",
+        "frase_filosofica": "La oscuridad es mi amiga, el miedo mi juego."
+    }
 ]
